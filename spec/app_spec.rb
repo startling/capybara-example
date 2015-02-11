ce24c0feb7c2f550f815e84713b0d63134033662
tim dixon <tdixon51793@gmail.com>
2015-02-10 16:14:47 -0800

    Make assertions about its length.

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -4,6 +4,7 @@ feature "getting a timestamp" do
   scenario "clicking the button gets a timestamp" do
     visit '/'
     find('div.text-button').click
+    expect(find('div.text-button').text.length).to be > 30
     expect(page).to have_selector('div.text-button.done')
   end
 end
