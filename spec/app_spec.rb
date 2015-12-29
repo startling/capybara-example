ed5e0b1a54d0553afce21e3b3776a5f1073d654d
Lizzie Dixon <_@lizzie.io>
2015-12-29 10:23:51 -0800

    Make this flaky

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -4,6 +4,6 @@ feature "getting a timestamp" do
   scenario "clicking the button gets a timestamp" do
     visit '/'
     find('div.time-button').click
-    expect(page).to have_selector('div.time-button.done')
+    find('div.time-button').text.should include("The time is")
   end
 end
